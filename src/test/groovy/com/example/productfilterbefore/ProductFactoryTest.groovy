4f928e4953f8bf78ca600e53c610a45040b6f797
weijian.duan <weijian.duan@exxonmobil.com>
2022-08-16 14:54:56 +0800

    [live session] fix Constructor smell: initialize objects in setup() method

--- src/test/groovy/com/example/productfilterbefore/ProductFactoryTest.groovy
+++ src/test/groovy/com/example/productfilterbefore/ProductFactoryTest.groovy
@@ -6,14 +6,16 @@ import spock.lang.Specification
 class ProductFactoryTest extends Specification {
 
   def productFactory = new ProductFactory()
+  def products
+
+  void setup() {
+    products = List.of(
+        new Product("RED", "BIG", "BIG RED PRODUCT", 10.00, 10.00, null, "RECTANGLE"),
+        new Product("BLUE", "SMALL", "SMALL BLUE PRODUCT", 20.00, 20, null, "RECTANGLE"),
+        new Product("RED", "MEDIUM", "MEDIUM RED PRODUCT", null, null, 10, "CIRCLE"))
+  }
 
   def "should filter product by red color"() {
-    given:
-    def products = List.of(
-        new Product("RED", "BIG", "BIG RED PRODUCT", 10, 10, null, "Rectangle"),
-        new Product("BLUE", "SMALL", "SMALL BLUE PRODUCT", 20, 20, null, "Triangle"),
-        new Product("RED", "MEDIUM", "MEDIUM RED PRODUCT", null, null, 10, "Circle"))
-
     when:
     def result = productFactory.filter(products, "RED", "BIG")
 
@@ -23,15 +25,10 @@ class ProductFactoryTest extends Specification {
   }
 
   def "should calculate product area"() {
-    given:
-    def products = List.of(
-        new Product("RED", "BIG", "BIG RED PRODUCT",10.00, 10.00,null, "RECTANGLE"),
-        new Product("RED", "BIG", "BIG RED PRODUCT",null,null,10.00, "CIRCLE"))
-
     when:
     def result = productFactory.calculateArea(products)
 
     then:
-    Math.round(result) == 414
+    Math.round(result) == 814
   }
 }
